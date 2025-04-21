6643b8b40d23c48bd5a830e32cdee8ca652d1d4f
henrygoodin <143525282+henrygoodin@users.noreply.github.com>
2025-04-21 11:34:46 -0500

    Update
    Yes

--- server/index.js
+++ server/index.js
@@ -5,10 +5,14 @@ dotenv.config();
 import cors from 'cors';
 import debug from 'debug';
 import { contactRouter } from './contact.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const debugServer = debug('app:Server');
 const app = express();
 const port = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
@@ -41,6 +45,8 @@ app.options('*', cors());
 
 // Routes
 app.use('/api/contacts/', contactRouter);
+
+app.use('/CoachTaysWebsite', express.static(path.join(__dirname, 'CoachTaysWebsite')));
 
 // Start server
 app.listen(port, () => {
